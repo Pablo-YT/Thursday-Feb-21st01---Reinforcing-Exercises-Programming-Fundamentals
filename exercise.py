c4dce2374882ee78e1c92d4f7759adb6aa136f03
Josh Date <jjdate@live.com>
2019-02-27 15:29:10 -0500

    done

---
+++ exercise.py
@@ -0,0 +1,19 @@
+def draw_shape(options):
+  shape = ""
+
+  for r in range(options['rows']):
+    for c in range(options['cols']):
+      shape += options['char']
+
+    shape += "\n"
+    
+
+  return shape
+
+my_var = {'rows': 4, 'cols': 4, 'char': '*'}
+
+print(draw_shape(my_var))
+
+my_var2 = {'rows': 3, 'cols':10, 'char': '0'}
+
+print(draw_shape(my_var2))
